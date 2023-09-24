f13d4e2a57c770880e3f24b3fdbdcd3378369a80
amisha26 <amisha8299@gmail.com>
2023-09-24 09:11:58 -0400

    created social media again

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+// const express = require("express");
+// const app = express();
+// const morgan = require("morgan");
+// const helmet = require("helmet");
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+// const userRoute = require("./routes/users");
+// const authRoute = require("./routes/auth");
+
+// dotenv.config()
+
+// mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}).then(() => {
+//     console.log("db connected")
+// });
+
+// //middleware
+// app.use(express.json());
+// app.use(helmet());
+// app.use(morgan("common"));
+
+
+// app.use("/api/users", userRoute);
+// app.use("./api/auth", authRoute);
+
+// app.listen(8080, () => {console.log("Backend is running")});
+
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 
-
 router.get("/", (req, res) => {
-    res.send("hey it's user route");
-})
+    res.send("hi, it's user route");
+});
 
 module.exports = router
